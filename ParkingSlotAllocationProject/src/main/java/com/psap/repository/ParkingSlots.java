afb5af230478974a055eb22ecbd621224dbb6a7f
Pravin Sankar <pravinchamp98@gmail.com>
2021-06-15 14:10:27 +0530

    Delete ParkingSlots.java

--- ParkingSlotAllocationProject/src/main/java/com/psap/repository/ParkingSlots.java
+++
@@ -1,9 +0,0 @@
-package com.psap.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingSlotsRepository extends JpaRepository <ParkingSlots, Long>{
-
-}
