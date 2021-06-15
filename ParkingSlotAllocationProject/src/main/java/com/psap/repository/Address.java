7ba61767699200cdcc3de3f82cb5fb0fe69979d2
Pravin Sankar <pravinchamp98@gmail.com>
2021-06-15 14:05:08 +0530

    Update Address.java

--- ParkingSlotAllocationProject/src/main/java/com/psap/repository/Address.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/repository/Address.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Address extends JpaRepository <Address, Integer> {
+public interface AddressRepository extends JpaRepository <Address, Integer> {
 
 }
