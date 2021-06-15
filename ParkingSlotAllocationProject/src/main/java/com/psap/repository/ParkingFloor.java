cab0fbb6b1a16fbf12a046d229a8213578af45fe
Pravin Sankar <pravinchamp98@gmail.com>
2021-06-15 14:05:06 +0530

    Update ParkingFloor.java

--- ParkingSlotAllocationProject/src/main/java/com/psap/repository/ParkingFloor.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/repository/ParkingFloor.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParkingFloor extends JpaRepository<ParkingFloor, Integer> {
+public interface ParkingFloorRepository extends JpaRepository<ParkingFloor, Integer> {
 
 }
