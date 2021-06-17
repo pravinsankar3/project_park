e15445632088a34f2448d125263112d568ba8a00
Shabana-Shaikh <85644522+Shabana-Shaikh@users.noreply.github.com>
2021-06-17 15:13:48 +0530

    updated payment repo & created payment service impl

--- ParkingSlotAllocationProject/src/main/java/com/psap/repository/PaymentRepository.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/repository/PaymentRepository.java
@@ -1,11 +1,17 @@
 package com.psap.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.psap.model.Payment;
+import com.psap.model.User;
+import com.psap.model.Vehicle;
 
 @Repository
 public interface PaymentRepository extends JpaRepository <Payment, Long>{
+	List<Payment> findByPaymentDateTime( Date paymentDate );
 
 }
