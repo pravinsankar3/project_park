7e5e698d9b64bdf77d5e6c096cdbc9b1846e5a68
Pravin Sankar <pravinchamp98@gmail.com>
2021-06-17 20:39:16 +0530

    Update ParkingService.java

--- ParkingSlotAllocationProject/src/main/java/com/psap/service/ParkingService.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/service/ParkingService.java
@@ -2,6 +2,9 @@ package com.psap.service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import com.psap.exceptions.NoSuchParkingSlotException;
 import com.psap.exceptions.ParkingSlotNotAvailableException;
@@ -9,13 +12,14 @@ import com.psap.model.ParkingFloor;
 import com.psap.model.ParkingPremise;
 import com.psap.model.ParkingSlots;
 
+@Service
 public interface ParkingService {
 	public boolean checkAvailability(Date date, String time) throws ParkingSlotNotAvailableException;
 	public boolean bookParkingSlot(ParkingSlots slot) throws ParkingSlotNotAvailableException;
 	public boolean cancelParkingSlotBooking(ParkingSlots slot) throws NoSuchParkingSlotException;
 	
-	public List<ParkingSlots> getAllParkingSlotsByPremise(ParkingPremise parkingPremise);
-	public List<ParkingSlots> getAllParkingSlotsByFloor(ParkingFloor parkingFloor);
-	public ParkingSlots getParkingSlotsById(long parkingSlotId) throws NoSuchParkingSlotException;
+	public List<ParkingPremise> getAllParkingSlotsByPremise(ParkingPremise parkingPremise);
+	public Optional<ParkingSlots> getParkingSlotsById(long parkingSlotId);
+	
 	
 }
