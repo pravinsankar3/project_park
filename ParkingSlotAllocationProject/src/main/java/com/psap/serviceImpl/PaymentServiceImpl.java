cc311f5eafcc15dfba6caf52de2802441ae0bb0e
yashaswini698 <62784518+yashaswini698@users.noreply.github.com>
2021-06-17 22:40:07 +0530

    Add files via upload

--- ParkingSlotAllocationProject/src/main/java/com/psap/serviceImpl/PaymentServiceImpl.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/serviceImpl/PaymentServiceImpl.java
@@ -2,30 +2,56 @@ package com.psap.serviceImpl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.psap.exceptions.DuplicateParkingPremiseException;
+import com.psap.exceptions.DuplicateVehicleException;
+import com.psap.exceptions.NoSuchParkingPremiseException;
 import com.psap.exceptions.NoSuchPaymentFoundException;
+import com.psap.exceptions.NoSuchVehicleException;
 import com.psap.exceptions.PaymentFailureException;
+import com.psap.model.ParkingPremise;
 import com.psap.model.Payment;
+import com.psap.model.User;
+import com.psap.model.Vehicle;
+import com.psap.repository.PaymentRepository;
 import com.psap.service.PaymentService;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
+	@Autowired
+	PaymentRepository dao;
+	
 	@Override
 	public boolean parkingBookingPayment(Payment payment) throws PaymentFailureException {
 		// TODO Auto-generated method stub
-		return false;
+		Optional<Payment> p= dao.findById(payment.getPaymentId()) ;
+		if(p.isPresent())
+			throw new PaymentFailureException("Payment Fail");
+		dao.save(payment);
+		return true;
 	}
 
 	@Override
 	public Payment findPaymentById(long paymentId) throws NoSuchPaymentFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<Payment> p= dao.findById(paymentId);
+
+		dao.findById(paymentId);
+		if(p.isPresent())
+			throw new NoSuchPaymentFoundException("No payment for this id");
+		return p.get();
 	}
 
 	@Override
-	public List<Payment> findPaymentByDate(Date paymentDate) throws NoSuchPaymentFoundException {
+	public List<Payment> findPaymentByDate(Date paymentDate) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		List<Payment> paymentList = dao.findBypaymentDateTime(paymentDate);
+		return paymentList;
 	}
-
 }
