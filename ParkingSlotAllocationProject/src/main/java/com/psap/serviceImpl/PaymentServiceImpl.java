2cbe8c7d4d8c77ff06fc975e9e64f14ba85caf4a
yashaswini698 <wini.c7@gmail.com>
2021-06-20 22:03:40 +0530

    Update PaymentServiceImpl.java

--- ParkingSlotAllocationProject/src/main/java/com/psap/serviceImpl/PaymentServiceImpl.java
+++ ParkingSlotAllocationProject/src/main/java/com/psap/serviceImpl/PaymentServiceImpl.java
@@ -17,6 +17,7 @@ public class PaymentServiceImpl implements PaymentService {
 	@Autowired
 	PaymentRepository dao;
 	
+	//Parking Booking payment
 	@Override
 	public boolean parkingBookingPayment(Payment payment) throws PaymentFailureException {
 		// TODO Auto-generated method stub
@@ -27,9 +28,9 @@ public class PaymentServiceImpl implements PaymentService {
 		return true;
 	}
 
+	// Check for payment by id
 	@Override
 	public Optional<Payment> findPaymentById(long paymentId) throws NoSuchPaymentFoundException {
-		// TODO Auto-generated method stub
 		Optional<Payment> p= dao.findById(paymentId);
 		if(!p.isPresent())
 			throw new NoSuchPaymentFoundException("No payment for this id");
